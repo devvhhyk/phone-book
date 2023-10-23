97ac878905b5fa7238782c4614fcbfbb4401d369
unknown <hayu6944@gmail.com>
2023-10-23 19:05:46 +0900

    first commit

---
+++ src/component/SearchBox.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import styled from "styled-components";
+
+const SearchForm = styled(Form)`
+  margin-top: 30px;
+`;
+
+const SearchBox = () => {
+  let [keyword, setKeyword] = useState("");
+  let dispatch = useDispatch();
+  let { contact } = useSelector((state) => state);
+  const searchByName = (event) => {
+    event.preventDefault();
+    dispatch({ type: "SEARCH_BY_USERNAME", payload: { keyword } });
+  };
+  return (
+    <SearchForm onSubmit={searchByName} className="search-form">
+      <Row>
+        <Col>
+          <Form.Control
+            type="text"
+            placeholder="키워드를 입력하세요"
+            onChange={(event) => setKeyword(event.target.value)}
+          />
+        </Col>
+        <Col>
+          <Button type="submit">찾기</Button>
+        </Col>
+      </Row>
+    </SearchForm>
+  );
+};
+
+export default SearchBox;
